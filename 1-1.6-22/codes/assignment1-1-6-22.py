a984a32722dc421daa08515399aaea153bd7ed4d
ysiddhanth <siddhanthyellanki.07@gmail.com>
2024-08-27 23:21:54 +0530

    write wtvr

--- 1-1.6-22/codes/assignment1-1-6-22.py
+++ 1-1.6-22/codes/assignment1-1-6-22.py
@@ -18,58 +18,30 @@ from line.funcs import *
 from triangle.funcs import *
 from conics.funcs import circ_gen
 
-roots = np.loadtxt("lambda.dat")
-#roots are roots[0] = -2; roots[1] and roots[2]
-k = roots[0]
-A = np.array(([k, 1,2])).reshape(-1,1) 
-B = np.array(([1,k, -1])).reshape(-1,1)  
-C = np.array(([2,-1, k])).reshape(-1,1)  
 
-P1 = np.array(([k, 1,2]))
-P2 = np.array(([1,k, -1]))
-P3 = np.array(([2,-1, k]))
-v1 = P2 - P1
-v2 = P3 - P1
+#Given points
+A = np.array(([2, -3,4])).reshape(-1,1) 
+B = np.array(([-1, 2,1])).reshape(-1,1) 
+C = np.array(([0,1/3, 2])).reshape(-1,1)  
 
-# Compute the normal vector (cross product of v1 and v2)
-normal = np.cross(v1, v2)
-
-# Plane equation: ax + by + cz = d
-# where [a, b, c] is the normal vector and d is calculated using one of the points
-a, b, c = normal
-d = np.dot(normal, P1)
 
 # Create a figure and a 3D Axes
 fig = plt.figure(figsize=(8, 6))
 ax = fig.add_subplot(111, projection='3d')
 
-
-
-# Generate grid points for x and y
-x = np.linspace(-5, 5, 50)
-y = np.linspace(-5, 5, 50)
-X, Y = np.meshgrid(x, y)
-
-# Calculate corresponding z values for each (x, y) pair to satisfy the plane equation
-Z = (-a*X - b*Y - d) / c
-
-# Plot the plane
-ax.plot_surface(X, Y, Z, alpha=0.5)
-#ax.plot_surface(X, Y, Z, alpha=0.5,color="grey")
-
 #Generating all lines
 x_BC = line_gen(B,C)
-x_AB = line_gen(A,B)
+x_AB = line_gen(B,A)
 x_AC = line_gen(A,C)
 
 
 #Plotting all lines
 ax.plot(x_BC[0,:],x_BC[1,:], x_BC[2,:],label='$BC$')
-ax.plot(x_AC[0,:],x_AC[1,:], x_AC[2,:],label='$AC$')
 ax.plot(x_AB[0,:],x_AB[1,:], x_AB[2,:],label='$AB$')
+ax.plot(x_AC[0,:],x_AC[1,:], x_AC[2,:],label='$AC$')
 
 # Scatter plot
-colors = np.arange(2, 5)  # Example colors
+colors = np.arange(1, 4)  # Example colors
 tri_coords = np.block([A, B, C])  # Stack A, B, C vertically
 ax.scatter(tri_coords[0, :], tri_coords[1, :], tri_coords[2, :], c=colors)
 vert_labels = ['A', 'B', 'C']
@@ -77,19 +49,13 @@ vert_labels = ['A', 'B', 'C']
 for i, txt in enumerate(vert_labels):
     # Annotate each point with its label and coordinates
     ax.text(tri_coords[0, i], tri_coords[1, i], tri_coords[2, i], f'{txt}\n({tri_coords[0, i]:.02f}, {tri_coords[1, i]:.02f}, {tri_coords[2, i]:.02f})',
-             fontsize=10, ha='center', va='baseline')
-
+             fontsize=12, ha='center', va='bottom')
 
 ax.spines['top'].set_color('none')
 ax.spines['left'].set_position('zero')
 ax.spines['right'].set_color('none')
 ax.spines['bottom'].set_position('zero')
 
-# Set limits and aspect ratio to magnify the plane
-ax.set_xlim(-4, 4)  # Adjust limits based on your data
-ax.set_ylim(-4, 4)  # Adjust limits based on your data
-ax.set_zlim(-4, 4)  # Adjust limits based on your data
-ax.set_box_aspect([1,1,1])  # Equal aspect ratio for x, y, and z axes
 '''
 ax.spines['left'].set_visible(False)
 ax.spines['right'].set_visible(False)
@@ -99,7 +65,9 @@ plt.xlabel('$x$')
 plt.ylabel('$y$')
 plt.legend(loc='best')
 '''
+plt.xlabel('$X$-Axis')
+plt.ylabel('$Y$-Axis')
 plt.grid() # minor
 plt.axis('equal')
-plt.title('Verifying λ = -2', loc = 'right', pad = 15)
+plt.title('Showing that A,B,C are collinear', loc = 'right', pad = 15)
 plt.show()
