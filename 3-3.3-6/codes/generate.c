81e16bd7b9b9b9cb47a706aa2ad774f45aa04db9
ysiddhanth <siddhanthyellanki.07@gmail.com>
2024-09-23 22:46:27 +0530

    3.3.3.6

--- 3-3.3-6/codes/generate.c
+++ 3-3.3-6/codes/generate.c
@@ -2,7 +2,15 @@
 #include <math.h>
 #include <stdlib.h>
 #include "libs/matfun.h"
-
+float **calcpoints(double **A, double **B, double **AB){
+	AB[0][0]= A[0][0]; AB[1][0]= A[1][0];
+	AB[0][51]= B[0][0]; AB[1][51]= B[1][0];
+        for(int i = 1; i<=50; i+=1){
+          float j = (float) i/50;
+          AB[0][i]= AB[0][0] + j*(AB[0][51]- AB[0][0]);
+          AB[1][i]= AB[1][0] + j*(AB[1][51]- AB[1][0]);
+        }
+}
 int main(void) {
     FILE *f = fopen("output.dat", "w");
 
@@ -28,17 +36,28 @@ int main(void) {
     double **vC = createMat(2, 1);
     vC[0][0] = a;
     vC[1][0] = 0;
+    double **AB,**BC,**CA;
+    AB = createMat(3,52);
+    BC = createMat(3,52);
+    CA = createMat(3,52); 
+    calcpoints(vA,vB,AB);
+    calcpoints(vB,vC,BC);
+    calcpoints(vC,vA,CA);
+    FILE *file;
+    file = fopen("values.dat", "w");
 
-    // Write the coordinates to a file
-    for (int i = 0; i < 2; i++) {
-        fprintf(f, "%lf\n", vA[i][0]);
+    if (file == NULL) {
+      	printf("Error opening file!\n");
+      	return 1;
     }
-    for (int i = 0; i < 2; i++) {
-        fprintf(f, "%lf\n", vB[i][0]);
-    }
-    for (int i = 0; i < 2; i++) {
-        fprintf(f, "%lf\n", vC[i][0]);
-    }
+    fprintf(file, "xab yab xbc ybc xca yca\n");
+    for(int i=0; i<=51; i+=1){
+      fprintf(file, "%.02lf %.02lf %.02lf %.02lf %.02lf %.02lf\n", AB[0][i],AB[1][i], BC[0][i],BC[1][i],CA[0][i],CA[1][i]);
+      }
+
+    fclose(file);
+    printf("Results have been written to values.dat\n");
+    return 0;
     
 
     return 0;
--- 3-3.3-6/codes/plot.py
+++ 3-3.3-6/codes/plot.py
@@ -1,51 +1,75 @@
-import matplotlib.pyplot as plt
+#Code by GVV Sharma
+#July 22, 2024
+#released under GNU GPL
+#Line 
+
+
+import sys                                          #for path to external scripts
+sys.path.insert(0, '/home/ysiddhanth/Documents/matgeo/codes/CoordGeo')        #path to my scripts
 import numpy as np
+import mpmath as mp
+import numpy.linalg as LA
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
 
-# Generates a line from point A to point B
-def line_gen(A,B):
-  len =10
-  dim = A.shape[0]
-  x_AB = np.zeros((dim,len))
-  lam_1 = np.linspace(0,1,len)
-  for i in range(len):
-    temp1 = A + lam_1[i]*(B-A)
-    x_AB[:,i]= temp1.T
-  return x_AB
+#local imports
+from line.funcs import *
+from triangle.funcs import *
+from conics.funcs import circ_gen
 
+#Direction vector
+#Given points
 
-# Points A, B and C
-A, B, C = np.loadtxt("output.dat").reshape(-1, 2, 1)
+data = np.genfromtxt('values.dat', delimiter=' ', names=True)
+xab = data['xab']
+yab = data['yab']
+xbc = data['xbc']
+ybc = data['ybc']
+xca = data['xca']
+yca = data['yca']
 
-# Plot AB, BC, and CA
-x_AB = line_gen(A, B)
-x_BC = line_gen(B, C)
-x_CA = line_gen(C, A)
-plt.plot(x_AB[0,:], x_AB[1,:], label="AB")
-plt.plot(x_BC[0,:], x_BC[1,:], label="BC")
-plt.plot(x_CA[0,:], x_CA[1,:], label="CA")
+A = np.array(([xab[0], yab[0]])).reshape(-1,1) 
+B = np.array(([xbc[0], ybc[0]])).reshape(-1,1) 
+C = np.array(([xca[0], yca[0]])).reshape(-1,1) 
+AB = np.vstack(([xab, yab]))
+BC = np.vstack(([xbc, ybc]))
+CA = np.vstack(([xca, yca]))
+colors = np.arange(1,4)
+#Labeling the coordinates
+tri_coords = np.block([A,B,C])
+plt.scatter(tri_coords[0,:], tri_coords[1,:], c=colors)
+plt.plot(AB[0, :], AB[1, :], label='$AB$')
+plt.plot(BC[0, :], BC[1, :], label='$BC$')
+plt.plot(CA[0, :], CA[1, :], label='$CA$')
+vert_labels = ['A','B','C']
+for i, txt in enumerate(vert_labels):
+    #plt.annotate(txt, # this is the text
+    plt.annotate(f'{txt}\n({tri_coords[0,i]:.2f}, {tri_coords[1,i]:.2f})',
+                 (tri_coords[0,i], tri_coords[1,i]), # this is the point to label
+                 textcoords="offset points", # how to position the text
+                 xytext=(-30,5), # distance from text to points (x,y)
+                 ha='center') # horizontal alignment can be left, right or center
 
-# Plot the points
-colors = np.arange(1, 4)
-p = np.block([A, B, C])
-plt.scatter(p[0, :], p[1, :], c=colors)
+# use set_position
+ax = plt.gca()
+ax.spines['top'].set_color('none')
+ax.spines['left'].set_position('zero')
+ax.spines['right'].set_color('none')
+ax.spines['bottom'].set_position('zero')
+'''
+ax.spines['left'].set_visible(False)
+ax.spines['right'].set_visible(False)
+ax.spines['top'].set_visible(False)
+ax.spines['bottom'].set_visible(False)
+plt.xlabel('$x$')
+plt.ylabel('$y$')
+plt.legend(loc='best')
+'''
 
-# Labels and their coordinates
-points = {
-    'A': A,
-    'B': B,
-    'C': C,
-}
-
-# Label the points
-for label, point in points.items():
-    plt.text(
-       point[0, 0], point[1, 0],
-       f"{label}\n({point[0,0]:.2f}, {point[1,0]:.2f})",
-       fontsize=12, ha="center", va="bottom"
-    )
-
-# Enable grid
-plt.grid()
-
-# Save the figure
+plt.xlabel('$X$-Axis')
+plt.ylabel('$Y$-Axis')
+plt.grid() # minor
+plt.axis('equal')
+plt.title('Drawing triangle ABC', loc = 'right', pad = 15)
+#plt.show()
 plt.savefig('../figs/fig.pdf')
